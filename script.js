d153f18413295081383f7cea8ec3067098d1ce78
weedythepooh <marian.krasnoludek@gmail.com>
2023-11-06 18:46:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$('document').ready(function () {
+    $('#menu').hover(function () {
+        console.log("wykryto");
+        $('.retractingElement').slideDown('slow');
+    }, function () {
+        $('.retractingElement').slideUp('slow');
+    });
+    $(window).on("resize", function () {
+        if (window.innerHeight > window.innerWidth) {
+            console.log("TAK");
+            $('.menuElement').hide();
+        } else {
+            $('.menuElement').show();
+        }
+
+    });
+    $('#mobileHamburgerICO').hover(function () {
+    $('.menuElement').slideDown('slow');
+    }, function () {
+    $('.menuElement').slideUp('slow');
+    });
+
+
+
+
+}); 
